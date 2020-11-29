841916de01c00eb334f0d5c261d0814c450ad1d8
dylan <466879168@qq.com>
2020-11-29 19:45:11 +0800

    change

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,9 +1,30 @@
 import React, { memo } from 'react';
+import {DiscoverWrapper,TopMenu} from './style'
+import {renderRoutes} from 'react-router-config'
+import {NavLink} from 'react-router-dom'
+import {dicoverMenu} from '@/services/local-data'
 
-export default memo(function MYDiscover() {
+
+export default memo(function MYDiscover(props) {
+  const {route}=props
   return (
-    <div>
-      <h2>MYDiscover</h2>
-    </div>
+    <DiscoverWrapper>
+      <div className="top">
+        <TopMenu className="wrap-v1">
+          {
+            dicoverMenu.map((item,index)=>{
+              return (
+                <div className="item" key={item.title}>
+                  <NavLink to={item.link}>
+                    {item.title}
+                  </NavLink>
+                </div>
+              )
+            })
+          }
+        </TopMenu>
+      </div>
+      {renderRoutes(route.routes)}
+    </DiscoverWrapper>
   )
 })
